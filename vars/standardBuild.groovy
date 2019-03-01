d727f4c9f6834a2a96925247bf786934de5a9827
Somesh Yadav <yadavsms@gmail.com>
2019-03-01 17:49:44 +0530

    updating post section

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -1,16 +1,7 @@
 def call(Map config) {
     node {
-        def app = config.app
         stage('Checkout') {
             checkout scm
-        }
-        stage("Build image") {
-            app = docker.build("config.environment")
-        }
-        stage('Push Image') {
-            withDockerRegistry(registry: [url: 'https://index.docker.io/v1/', credentialsId:'docker-hub-credentials']) {
-                app.push("latest")
-            }
         }
         stage('Main') {
             docker.image(config.environment).inside {
@@ -18,9 +9,9 @@ def call(Map config) {
             }
         }
         stage('Post') {
-            docker.image(config.environment).inside {
-                sh config.postScript
-            } 
+            // docker.image(config.environment).inside {
+            sh config.postScript
+            // } 
         }
     }
 }
